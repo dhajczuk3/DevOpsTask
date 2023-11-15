fe66cc751f818e7c292e95a72e1ced4a1d6f168f
dhajczuk3 <dhajczuk@gmail.com>
2023-11-15 20:21:54 +0000

    v.2.1 - major bug fixes at lines 6-8

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,38 +1,22 @@
 import java.util.Scanner;
 
-class Dec2Hex
-{
-    public static void main(String args[])
- {
+class Dec2Hex {
+    public static void main(String args[]) {
         // Check if an argument is provided
         if (args.length < 1) {
             System.out.println("Error: No input provided.");
             return; // Exit the program
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Valuee " + num + " to Hex...");
-
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
         }
 
         try {
-            // Parse the input to an integer
             int Arg1 = Integer.parseInt(args[0]);
-            char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
             int rem;
-            String hexadecimal="";
+            String hexadecimal = "";
 
             System.out.println("Converting the Decimal Value " + Arg1 + " to Hex...");
 
-            while(Arg1 != 0)
-            {
+            while (Arg1 != 0) {
                 rem = Arg1 % 16;
                 hexadecimal = ch[rem] + hexadecimal;
                 Arg1 = Arg1 / 16;
