e6b648a935116d2ce9de2f55fe9a2ff07f9e594b
dhajczuk3 <dhajczuk@gmail.com>
2023-11-16 03:51:00 +0000

    v3.0 - Fix SonarQube issues in Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,30 +1,35 @@
-import java.util.Scanner;
+package com.example.conversion;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class Dec2Hex {
-    public static void main(String args[]) {
-        // Check if an argument is provided
+    private static final Logger logger = LoggerFactory.getLogger(Dec2Hex.class);
+    private static final char[] DIGIT_TO_HEX_MAPPING = {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+    };
+    private static final int HEX_BASE = 16;
+
+    public static void main(String[] args) {
         if (args.length < 1) {
-            System.out.println("Error: No input provided.");
-            return; // Exit the program
+            logger.error("Error: No input provided.");
+            return;
         }
 
         try {
-            int Arg1 = Integer.parseInt(args[0]);
-            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-            int rem;
-            String hexadecimal = "";
+            int decimalNumber = Integer.parseInt(args[0]);
+            StringBuilder hexadecimal = new StringBuilder();
+            logger.info("Converting the Decimal Value {} to Hex...", decimalNumber);
 
-            System.out.println("Converting the Decimal Value " + Arg1 + " to Hex...");
-
-            while (Arg1 != 0) {
-                rem = Arg1 % 16;
-                hexadecimal = ch[rem] + hexadecimal;
-                Arg1 = Arg1 / 16;
+            while (decimalNumber != 0) {
+                int remainder = decimalNumber % HEX_BASE;
+                hexadecimal.insert(0, DIGIT_TO_HEX_MAPPING[remainder]);
+                decimalNumber = decimalNumber / HEX_BASE;
             }
 
-            System.out.println("Hexadecimal representation is: " + hexadecimal);
+            logger.info("Hexadecimal representation is: {}", hexadecimal);
         } catch (NumberFormatException e) {
-            System.out.println("Error: The input value is not an integer.");
+            logger.error("Error: The input value is not an integer.");
         }
     }
 }
