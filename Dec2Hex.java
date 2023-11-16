493cb2946bdb80556a0e8d3734ec4eb62a3fb5d1
dhajczuk3 <dhajczuk@gmail.com>
2023-11-16 04:07:02 +0000

    v3.2 - Part 3 of fixing SonarQube issues in Dec2Hex.java - simplified the code

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,29 +1,27 @@
-package com.example.conversion;
-
 class Dec2Hex {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        // Check if an argument is provided
         if (args.length < 1) {
-            log("Error: No input provided.");
-            return;
+            System.err.println("Error: No input provided.");
+            return; // Exit the program
         }
         try {
             int decimalNumber = Integer.parseInt(args[0]);
-            char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
             StringBuilder hexadecimal = new StringBuilder();
-            log("Converting the Decimal Value " + decimalNumber + " to Hex...");
+            System.out.println("Converting the Decimal Value " + decimalNumber + " to Hex...");
+
             while (decimalNumber != 0) {
-                int rem = decimalNumber % 16;
-                hexadecimal.insert(0, ch[rem]);
+                int remainder = decimalNumber % 16;
+                hexadecimal.insert(0, hexDigits[remainder]);
                 decimalNumber = decimalNumber / 16;
             }
-            log("Hexadecimal representation is: " + hexadecimal);
+
+            System.out.println("Hexadecimal representation is: " + hexadecimal);
         } catch (NumberFormatException e) {
-            log("Error: The input value is not an integer.");
+            System.err.println("Error: The input value is not an integer.");
         }
     }
-
-    private static void log(String message) {
-        System.out.println(message);
-    }
 }
+
 
